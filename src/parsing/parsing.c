36b1fe6cf9a9a5a90a7a811afefeaa4789989542
Soufiane Essarhir <soufiane.essarhir@edu.uiz.ac.ma>
2024-12-18 16:05:47 +0100

    printing the scene

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 21:09:07 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/12/18 15:38:08 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/12/18 16:04:21 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,6 @@ int process_C(char **inf , t_tscene *t_scene)
         return (1);
     if (t_scene->is_c_set == true)
         return (printf("Error multiple Cameras\n"),1);
-    printf("here\n");
     t_scene->camera.fov = atoi(inf[3]);
     if (!parse_crd(inf[1],&t_scene->camera.x , &t_scene->camera.y , &t_scene->camera.z) 
     || !parse_crd(inf[2], &t_scene->camera.vx, &t_scene->camera.vy, &t_scene->camera.vz)
@@ -57,6 +56,7 @@ int process_L(char **inf , t_tscene *t_scene)
      !parse_rgb(inf[3], &t_scene->light.r, &t_scene->light.g, &t_scene->light.b) ||
       t_scene->light.bratio < 0 || t_scene->light.bratio > 1)
         return (1);
+    t_scene->is_l_set = true;
     return (0);
 }
 
@@ -153,6 +153,92 @@ void int_tsceen(t_tscene *tscene)
     tscene->is_l_set = false;
     tscene->is_a_set = false;
 }
+void print_scene(t_tscene *tscene)
+{
+    t_tsphere *sphere;
+    t_tplane *plane;
+    t_tcylinder *cylinder;
+
+    sphere = tscene->sphere;
+    plane = tscene->plane;
+    cylinder = tscene->cylinder;
+    printf("Ambient\n");
+    printf("ratio : %f\n", tscene->ambient.ratio);
+    printf("r : %d\n", tscene->ambient.r);
+    printf("g : %d\n", tscene->ambient.g);
+    printf("b : %d\n", tscene->ambient.b);
+    printf("Camera\n");
+    printf("x : %f\n", tscene->camera.x);
+    printf("y : %f\n", tscene->camera.y);
+    printf("z : %f\n", tscene->camera.z);
+    printf("vx : %f\n", tscene->camera.vx);
+    printf("vy : %f\n", tscene->camera.vy);
+    printf("vz : %f\n", tscene->camera.vz);
+    printf("fov : %d\n", tscene->camera.fov);
+    printf("Light\n");
+    printf("x : %f\n", tscene->light.x);
+    printf("y : %f\n", tscene->light.y);
+    printf("z : %f\n", tscene->light.z);
+    printf("bratio : %f\n", tscene->light.bratio);
+    printf("r : %d\n", tscene->light.r);
+    printf("g : %d\n", tscene->light.g);
+    printf("b : %d\n", tscene->light.b);
+    printf("Sphere size : %d\n", tscene->sphere_size);
+    printf("Plane size : %d\n", tscene->plane_size);
+    printf("Cylinder size : %d\n", tscene->cylinder_size);
+    printf("is_c_set : %d\n", tscene->is_c_set);
+    printf("is_l_set : %d\n", tscene->is_l_set);
+    printf("is_a_set : %d\n", tscene->is_a_set);
+    printf("Objects\n");
+   
+    while (plane)
+    {
+         printf("Plane\n");
+        printf("x : %f\n", plane->x);
+        printf("y : %f\n", plane->y);
+        printf("z : %f\n", plane->z);
+        printf("vx : %f\n", plane->vx);
+        printf("vy : %f\n", plane->vy);
+        printf("vz : %f\n", plane->vz);
+        printf("r : %d\n", plane->r);
+        printf("g : %d\n", plane->g);
+        printf("b : %d\n", plane->b);
+        plane = plane->next;
+    }
+    while (sphere)
+    {
+         printf("Sphere\n");
+        printf("x : %f\n", sphere->x);
+        printf("y : %f\n", sphere->y);
+        printf("z : %f\n", sphere->z);
+        printf("diameter : %f\n", sphere->diameter);
+        printf("r : %d\n", sphere->r);
+        printf("g : %d\n", sphere->g);
+        printf("b : %d\n", sphere->b);
+        sphere = sphere->next;
+    }
+    
+    while (cylinder)
+    {
+        printf("Cylinder\n");
+        printf("x : %f\n", cylinder->x);
+        printf("y : %f\n", cylinder->y);
+        printf("z : %f\n", cylinder->z);
+        printf("vx : %f\n", cylinder->vx);
+        printf("vy : %f\n", cylinder->vy);
+        printf("vz : %f\n", cylinder->vz);
+        printf("diameter : %f\n", cylinder->diameter);
+        printf("height : %f\n", cylinder->height);
+        printf("r : %d\n", cylinder->r);
+        printf("g : %d\n", cylinder->g);
+        printf("b : %d\n", cylinder->b);
+        cylinder = cylinder->next;
+    }
+    
+            
+
+}
+
 void   process_flie(char **av , t_scene *scene)
 {
     int fd;
@@ -172,6 +258,7 @@ void   process_flie(char **av , t_scene *scene)
         splil_line(buffer, &tscene);
         ret = read(fd, buffer, BUFFER_SIZE);
     }
+    print_scene(&tscene);
     free(buffer);
     (void)scene;
     close(fd);
