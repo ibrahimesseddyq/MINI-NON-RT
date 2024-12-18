e4d5bec0cc824b213a9359d62fdcdbc757231b33
Soufiane Essarhir <soufiane.essarhir@edu.uiz.ac.ma>
2024-12-18 16:39:03 +0100

    add tests

--- tester.sh
+++ tester.sh
@@ -3,6 +3,14 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 RESET='\033[0m'
+#file to test
+file1=./testfiles/sphere_tests.rt
+file2=./testfiles/ambiant_tests.rt
+file3=./testfiles/camera_tests.rt
+file4=./testfiles/cylinder_tests.rt
+file5=./testfiles/light_tests.rt
+file6=./testfiles/plane_tests.rt
+
 #check if the program is compiled
 if [ ! -f "minirt" ]; then
     make > /dev/null
@@ -18,4 +26,5 @@ while read -r line; do
         # break
     fi
 done < $inputfile
+    echo -e "${GREEN}Test passed${RESET}"
 rm temp.txt > /dev/null
