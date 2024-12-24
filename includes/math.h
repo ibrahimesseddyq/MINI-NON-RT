d60f18951e879c80699937879542149658f12a33
Soufiane Essarhir <soufiane.essarhir@edu.uiz.ac.ma>
2024-12-24 19:39:57 +0100

    draw the spher

--- includes/math.h
+++ includes/math.h
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 17:01:10 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/12/24 17:35:24 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/12/24 19:33:53 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,17 @@
 
 #include "defined.h"
 #include <math.h>
+#include <stdbool.h>
+typedef struct s_intersection
+{
+    bool hit;
+    FLOAT distance;
+    t_point point;
+    t_vector normal;
+    FLOAT t;
+} t_intersection;
+
+
 
 t_vector vector_add(t_vector *a, t_vector *b);
 t_vector vector_sub(t_vector *a, t_vector *b);
