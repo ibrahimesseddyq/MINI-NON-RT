aec59ebcdef009ca6c35a6813d8aaee57c6aed3c
Soufiane Essarhir <soufiane.essarhir@edu.uiz.ac.ma>
2024-12-24 18:48:00 +0100

    start the window

--- src/rendering/render.c
+++ src/rendering/render.c
@@ -6,17 +6,58 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 17:44:32 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/12/24 17:51:30 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/12/24 18:47:08 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minirt.h"
 
 
+void my_mlx_pixel_put(t_data *img, int x, int y, int color)
+{
+    char *dst;
 
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    *(unsigned int *)dst = color;
+}
 
+int trace_ray(t_ray *ray, t_scene *scene)
+{
+    return (0xFF0000);
+}
 
+void draw(t_scene *scene)
+{
+    int x;
+    int y;
+    FLOAT pixel_x;
+    FLOAT pixel_y;
+    t_vector direction;
+    t_ray ray;
+    FLOAT aspect_ratio;
+    FLOAT fov_scale;
 
+    aspect_ratio = (FLOAT)WIDTH / (FLOAT)HEIGHT;
+    fov_scale = tan((scene->camera.fov * M_PI / 180) / 2);
+    y = 0;
+    while (y < HEIGHT)
+    {
+        x = 0;
+        while (x < WIDTH)
+        {
+            pixel_x = (2 * ((x + 0.5) / WIDTH) - 1) * aspect_ratio * fov_scale;
+            pixel_y = (1 - 2 * ((y + 0.5) / HEIGHT)) * fov_scale;
+            direction.x = pixel_x;
+            direction.y = pixel_y;
+            direction.z = -1;
+            ray = (t_ray){scene->camera.position, vector_normalize(&direction)};
+            my_mlx_pixel_put(&scene->img, x, y, trace_ray(&ray, scene));
+            x++;
+        }
+        y++;
+    }
+    mlx_put_image_to_window(scene->mlx, scene->win, scene->img.img, 0, 0);
+}
 
 
 
@@ -25,11 +66,12 @@
 void render(t_scene *scene)
 {
    
-   scene->mlx = mlx_init();
+    scene->mlx = mlx_init();
     scene->win = mlx_new_window(scene->mlx, WIDTH, HEIGHT, "MiniRT");
     scene->img.img = mlx_new_image(scene->mlx, WIDTH, HEIGHT);
-    scene->img.addr = mlx_get_data_addr(scene->img.img, &scene->img.bits_per_pixel, &scene->img.line_length, &scene->img.endian);
-    
+    scene->img.addr = mlx_get_data_addr(scene->img.img,
+         &scene->img.bits_per_pixel, &scene->img.line_length, &scene->img.endian);
+    draw(scene);
     mlx_loop(scene->mlx);
     
 }
