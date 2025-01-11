64cf5be3fc9cff892382762b675e4421257a945b
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-01-11 18:20:20 +0100

    aa

--- mand/src/parsing/parsing.c
+++ mand/src/parsing/parsing.c
@@ -6,116 +6,13 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 21:09:07 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/01/11 17:30:55 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/01/11 18:15:30 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "./../../../minirt.h"
 
-int process_A(char **inf , t_tscene *tscene)
-{
-    if (!inf[1] || !inf[2] || inf[3])
-        return (1);
-    if (tscene->is_a_set == true)
-        return (printf("Error multiple Ambient lights\n"),1);
-    tscene->ambient.ratio = ft_atof(inf[1]);
-    if (!parse_rgb(inf[2],&tscene->ambient.color)
-    || tscene->ambient.ratio < 0 || tscene->ambient.ratio > 1)
-        return (1);
-    tscene->is_a_set = true;
-    return (0);
-}
-
-int process_C(char **inf , t_tscene *t_scene)
-{
-    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
-        return (1);
-    if (t_scene->is_c_set == true)
-        return (printf("Error multiple Cameras\n"),1);
-    t_scene->camera.fov = ft_atoi(inf[3]);
-    if (!parse_crd(inf[1],&t_scene->camera.position) 
-    || !parse_crd(inf[2], &t_scene->camera.direction))
-        return (1);
-    t_vector normalized_dir = vector_normalize(&t_scene->camera.direction);
-    t_scene->camera.direction.x = normalized_dir.x;
-    t_scene->camera.direction.y = normalized_dir.y;
-    t_scene->camera.direction.z = normalized_dir.z;
-    if (t_scene->camera.fov > 180 || t_scene->camera.fov < 0 || t_scene->camera.direction.x < -1 ||
-    t_scene->camera.direction.x > 1 || t_scene->camera.direction.y < -1 || t_scene->camera.direction.y > 1 || 
-    t_scene->camera.direction.z < -1 || t_scene->camera.direction.z > 1)
-        return (1);
-    t_scene->is_c_set = true;
-    return (0);
-}
-
-int process_L(char **inf , t_tscene *t_scene)
-{
-    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
-        return (1);
-    if (t_scene->is_l_set == true)
-        return (printf("Error multiple Lights\n"),1);
-    t_scene->light.bratio = ft_atof(inf[2]);
-    if (!parse_crd(inf[1], &t_scene->light.position) ||
-     !parse_rgb(inf[3], &t_scene->light.color) ||
-      t_scene->light.bratio < 0 || t_scene->light.bratio > 1)
-        return (1);
-
-    t_scene->is_l_set = true;
-    return (0);
-}
-
-int process_sp(char **inf ,t_tscene *t_scene)
-{
-    t_tsphere *new;
-
-    new = new_sphere();
-    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
-        return (1);
-    new->diameter = ft_atof(inf[2]);
-    if (!parse_crd(inf[1],&new->position) ||
-        !parse_rgb(inf[3], &new->color) ||
-         new->diameter == (float)INT_MIN)
-            return (1);
-    sphere_add_front(&t_scene->sphere, new);
-    t_scene->sphere_size++;
-    return (0);
-}
-
-int process_pl(char **inf , t_tscene *t_scene)
-{
-    t_tplane *new;
-
-    new = new_plane();
-    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
-        return (1);
-    if (!parse_crd(inf[1], &new->position) ||
-     !parse_crd(inf[2], &new->direction) ||
-     !parse_rgb(inf[3], &new->color))
-        return (1);
-    plane_add_front(&t_scene->plane, new);
-    t_scene->plane_size++;
-    return (0);
-}
-
-int process_cy(char **inf , t_tscene *t_scene)
-{
-    t_tcylinder *new;
-
-    new = new_cylinder();
-    if (!inf[1] || !inf[2] || !inf[3] || !inf[4] || !inf[5] || inf[6])
-        return (1);
-    new->diameter = ft_atof(inf[3]);
-    new->height = ft_atof(inf[4]);
-    if  (!parse_crd(inf[1], &new->position) ||
-     !parse_crd(inf[2], &new->direction) ||
-     !parse_rgb(inf[5], &new->color) ||
-      new->diameter == (float)INT_MIN || new->height == (float)INT_MIN)
-        return (1);
-    cylinder_add_front(&t_scene->cylinder, new);
-    t_scene->cylinder_size++;
-    return (0);
-}
 void splil_line(const char *line , t_tscene *tscene)
 {
     char **elm;
@@ -162,28 +59,28 @@ void int_tsceen(t_tscene *tscene)
     tscene->camera.direction.y = 0;
     tscene->camera.direction.z = 0;
 }
-void print_data(t_scene *scene)
-{
-    int i = 0;
-    while (scene->plane_count--)
-    {
-        printf("Plane id %d\n", scene->plane[i].id);
-        i++;
-    }
-    i = 0;
-    while (scene->sphere_count--)
-    {
-        printf("Sphere id %d\n", scene->sphere[i].id);
-        i++;
-    }
-    i = 0;
-    while (scene->cylinder_count--)
-    {
-        printf("Cylinder id %d\n", scene->cylinder[i].id);
-        i++;
-    }
+// void print_data(t_scene *scene)
+// {
+//     int i = 0;
+//     while (scene->plane_count--)
+//     {
+//         printf("Plane id %d\n", scene->plane[i].id);
+//         i++;
+//     }
+//     i = 0;
+//     while (scene->sphere_count--)
+//     {
+//         printf("Sphere id %d\n", scene->sphere[i].id);
+//         i++;
+//     }
+//     i = 0;
+//     while (scene->cylinder_count--)
+//     {
+//         printf("Cylinder id %d\n", scene->cylinder[i].id);
+//         i++;
+//     }
     
-}
+// }
 
 void copy_tscene(t_tscene *tscene, t_scene *scene)
 {
@@ -258,6 +155,5 @@ void   process_flie(char **av , t_scene *scene)
     }
     free(buffer);
     copy_tscene(&tscene, scene);
-    (void)scene;
     close(fd);
 }
---
+++ mand/src/parsing/scene_obj.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scene_obj.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 18:11:36 by sessarhi          #+#    #+#             */
+/*   Updated: 2025/01/11 18:14:08 by sessarhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./../../../minirt.h"
+
+int process_sp(char **inf ,t_tscene *t_scene)
+{
+    t_tsphere *new;
+
+    new = new_sphere();
+    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
+        return (1);
+    new->diameter = ft_atof(inf[2]);
+    if (!parse_crd(inf[1],&new->position) ||
+        !parse_rgb(inf[3], &new->color) ||
+         new->diameter == (float)INT_MIN)
+            return (1);
+    sphere_add_front(&t_scene->sphere, new);
+    t_scene->sphere_size++;
+    return (0);
+}
+
+int process_pl(char **inf , t_tscene *t_scene)
+{
+    t_tplane *new;
+
+    new = new_plane();
+    if (!inf[1] || !inf[2] || !inf[3] || inf[4])
+        return (1);
+    if (!parse_crd(inf[1], &new->position) ||
+     !parse_crd(inf[2], &new->direction) ||
+     !parse_rgb(inf[3], &new->color))
+        return (1);
+    plane_add_front(&t_scene->plane, new);
+    t_scene->plane_size++;
+    return (0);
+}
+
+int process_cy(char **inf , t_tscene *t_scene)
+{
+    t_tcylinder *new;
+
+    new = new_cylinder();
+    if (!inf[1] || !inf[2] || !inf[3] || !inf[4] || !inf[5] || inf[6])
+        return (1);
+    new->diameter = ft_atof(inf[3]);
+    new->height = ft_atof(inf[4]);
+    if  (!parse_crd(inf[1], &new->position) ||
+     !parse_crd(inf[2], &new->direction) ||
+     !parse_rgb(inf[5], &new->color) ||
+      new->diameter == (float)INT_MIN || new->height == (float)INT_MIN)
+        return (1);
+    cylinder_add_front(&t_scene->cylinder, new);
+    t_scene->cylinder_size++;
+    return (0);
+}
