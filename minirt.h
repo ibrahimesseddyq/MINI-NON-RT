126de812ae76ad742d0dae54add835676fe11ee2
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-01-11 17:36:02 +0100

    reordering the project and preparing for bonus

--- minirt.h
+++ minirt.h
@@ -6,20 +6,20 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 19:16:40 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/01/10 17:15:15 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/01/11 17:25:03 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-#include "./includes/objects.h"
-#include "./includes/tmpobjects.h"
-#include "./includes/defined.h"
-#include "./includes/math.h"
-#include "./includes/memory.h"
-#include "./includes/color.h"
-#include "./includes/includes.h"
+#include "./mand/includes/objects.h"
+#include "./mand/includes/tmpobjects.h"
+#include "./mand/includes/defined.h"
+#include "./mand/includes/math.h"
+#include "./mand/includes/memory.h"
+#include "./mand/includes/color.h"
+#include "./mand/includes/includes.h"
 
 bool  parse_crd(const char *str, t_vector *vector);
 bool	parse_rgb(const char *str ,t_color *color);
