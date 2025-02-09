88cbb0d39b04604782b66e68adaf1c6521779f50
Ibrahim Es-seddyq <ibes-sed@e1r4p13.1337.ma>
2025-02-09 16:55:06 +0100

    test cone

--- bonus/src/rendering/ray_cone.c
+++ bonus/src/rendering/ray_cone.c
@@ -6,7 +6,7 @@
 /*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 22:23:45 by ibes-sed          #+#    #+#             */
-/*   Updated: 2025/02/08 21:14:30 by ibes-sed         ###   ########.fr       */
+/*   Updated: 2025/02/09 16:54:44 by ibes-sed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,32 @@ void	calculate_cone_normal(t_scene *scene,
 		= vector_sub(&inter->cone_to_point, &inter->axis_scaled);
 	intersection->normal = vector_normalize(&inter->subs_vec);
 }
+t_vector cone_normal(t_cone *cone, t_vector *hit_point)
+{
+    t_vector to_hit;
+    t_vector axis_proj;
+    t_vector normal;
+	t_vector scaled_vector;
+    FLOAT k;
+
+    // Vector from vertex to hit point
+    to_hit = vector_sub(hit_point, &cone->vertex);
+
+    // Compute projection on axis
+    k = vector_dot(&to_hit, &cone->axis);
+    axis_proj = vector_scale(&cone->axis, k);
+
+    // Compute normal (adjusting for cone slant)
+    normal = vector_sub(&to_hit, &axis_proj);
+    k = tan(cone->angle * M_PI / 180.0); // Convert angle to radians
+	scaled_vector = vector_scale(&cone->axis, k * vector_length(&normal));
+    normal = vector_sub(&normal, &scaled_vector);
+
+    // Normalize the result
+    vector_normalize(&normal);
+    return normal;
+}
+
 
 bool	cone_intersection(t_scene *scene,
 		t_intersection *intersection, t_ray *ray)
@@ -57,10 +83,13 @@ bool	cone_intersection(t_scene *scene,
 			intersection->hit = true;
 			intersection->id = scene->cone[i].id;
 			intersection->distance = t;
+			intersection->material = scene->cone[i].material;
 			intersection->color = scene->cone[i].color;
 			tmp = vector_scale(&ray->direction, t);
 			intersection->point
 				= vector_add(&ray->origin, &tmp);
+			intersection->normal = cone_normal(&scene->cone[i], &intersection->point);
+			vector_normalize(&intersection->normal);
 		}
 	}
 	// printf("cone hit color[%f, %f, %f]\ndistance %f\n", intersection->color.r, intersection->color.g, intersection->color.b,intersection->distance);
