9dae8b792962a5a8f867e53eff541058c09fe7ba
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-02-10 19:10:57 +0100

    there is a problem in u , v calculation

--- bonus/src/rendering/cone_textures.c
+++ bonus/src/rendering/cone_textures.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cone_textures.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibes-sed <ibes-sed@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 18:06:09 by ibes-sed          #+#    #+#             */
-/*   Updated: 2025/02/08 19:43:44 by ibes-sed         ###   ########.fr       */
+/*   Updated: 2025/02/10 19:06:47 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@ void	handle_cone_maps(t_scene *scene,
 	t_texture_params *params, t_checker_config	*checker, int i)
 {
 	if (scene->cone[i].has_color_texture)
+	{
+		// printf("has color texture [%d]\n", scene->cone[i].has_color_texture);
+
 		*(params->texture_color) = sample_texture(&scene->cone[i].texture,
 				params->intersection->u, params->intersection->v, 1);
+		// printf("texture_color: %f %f %f\n", params->texture_color->r, params->texture_color->g, params->texture_color->b);
+	}
 	else
 		*(params->texture_color) = scene->cone[i].color;
 	if (scene->cone[i].has_bump_texture)
@@ -25,7 +30,9 @@ void	handle_cone_maps(t_scene *scene,
 			= calculate_bump_normal(&scene->cone[i].normal_texture,
 				params->intersection->u, params->intersection->v,
 				&params->intersection->normal);
-	*(params->set) = 1;
+		*(params->set) = 1;
+	// printf("is set [%d]\n", *(params->set));
+// printf("texture_color: %f %f %f\n", params->texture_color->r, params->texture_color->g, params->texture_color->b);
 }
 
 void	handle_cone_textures(t_scene *scene, t_texture_params *params)
