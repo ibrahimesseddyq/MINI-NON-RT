f22b425d1740f5eaa4476e584d660cdfca354753
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-02-11 20:23:12 +0100

    update and handl some norm errors

--- bonus/src/rendering/move.c
+++ bonus/src/rendering/move.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 22:23:33 by ibes-sed          #+#    #+#             */
-/*   Updated: 2025/02/11 20:04:39 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/02/11 20:10:17 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	move_light_or_camera(int *keys, t_scene *scene)
 {
 	int	i;
 	int	obj_id;
-	
+
 	i = 0;
 	if ((char)keys[1] == 'c')
 		move_camera(keys, &scene->camera);
