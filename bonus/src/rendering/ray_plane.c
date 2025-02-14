d80e2c4deb85b61d566b2207ee86544e11be0a84
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-02-14 10:48:39 +0100

    fix u and v calculation for plane

--- bonus/src/rendering/ray_plane.c
+++ bonus/src/rendering/ray_plane.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
-/*                                                                            */
+/*	                                                                        */
 /*                                                        :::      ::::::::   */
 /*   ray_plane.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 17:02:02 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/02/13 22:51:43 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/02/14 10:42:22 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,61 +30,49 @@ FLOAT	hit_plane(const t_vector *point, const t_vector *normal,
 	return (-1);
 }
 
-void cal_uv_basis(const t_vector *normal, t_vector *u_axis, t_vector *v_axis)
+void	cal_uv_basis(const t_vector *normal,
+		t_vector *u_axis, t_vector *v_axis)
 {
-    t_vector arbitrary;
+	t_vector	arbitrary;
 
-    // Initialize the arbitrary vector
-    if (fabs(normal->x) < 0.9)
-    {
-        arbitrary = (t_vector){1.0, 0.0, 0.0};
-    }
-    else if (fabs(normal->y) < 0.9)
-    {
-        arbitrary = (t_vector){0.0, 1.0, 0.0};
-    }
-    else
-    {
-        arbitrary = (t_vector){0.0, 0.0, 1.0};
-    }
-
-    // Calculate u_axis as the cross product of normal and arbitrary
-    *u_axis = vector_cross(normal, &arbitrary);
-    *u_axis = vector_normalize(u_axis);
-
-    // Calculate v_axis as the cross product of normal and u_axis
-    *v_axis = vector_cross(normal, u_axis);
-    *v_axis = vector_normalize(v_axis);
+	if (fabs(normal->x) < 0.9)
+		arbitrary = (t_vector){1.0, 0.0, 0.0};
+	else if (fabs(normal->y) < 0.9)
+		arbitrary = (t_vector){0.0, 1.0, 0.0};
+	else
+		arbitrary = (t_vector){0.0, 0.0, 1.0};
+	*u_axis = vector_cross(normal, &arbitrary);
+	*u_axis = vector_normalize(u_axis);
+	*v_axis = vector_cross(normal, u_axis);
+	*v_axis = vector_normalize(v_axis);
 }
 
-bool plane_intersection(const t_scene *scene, t_intersection *intersection, const t_ray *ray)
+bool	plane_intersection(const t_scene *scene,
+		t_intersection *intersection, const t_ray *ray)
 {
-    t_plane_intersection pint;
+	t_plane_intersection	pint;
 
-    pint.i = scene->plane_count;
-    while (pint.i--)
-    {
-        pint.t = hit_plane(&scene->plane[pint.i].position, &scene->plane[pint.i].direction, ray);
-        if (pint.t > 0 && pint.t < intersection->distance)
-        {
-            intersection->hit = true;
-            intersection->id = scene->plane[pint.i].id;
-            intersection->material = scene->plane[pint.i].material;
-            intersection->distance = pint.t;
-            intersection->color = scene->plane[pint.i].color;
-            intersection->normal = scene->plane[pint.i].direction;
-
-            pint.tmp = vector_scale(&ray->direction, pint.t);
-            intersection->point = vector_add(&ray->origin, &pint.tmp);
-
-            // Calculate UV basis vectors
-            cal_uv_basis(&scene->plane[pint.i].direction, &pint.u, &pint.v);
-
-            // Calculate UV coordinates
-            pint.hpr = vector_sub(&intersection->point, &scene->plane[pint.i].position);
-            intersection->u = vector_dot(&pint.hpr, &pint.u) * 0.1;
-            intersection->v = vector_dot(&pint.hpr, &pint.v) * 0.1;
-        }
-    }
-    return (intersection->hit);
+	pint.i = scene->plane_count;
+	while (pint.i--)
+	{
+		pint.t = hit_plane(&scene->plane[pint.i].position,
+				&scene->plane[pint.i].direction, ray);
+		if (pint.t > 0 && pint.t < intersection->distance)
+		{
+			intersection->hit = true;
+			intersection->id = scene->plane[pint.i].id;
+			intersection->material = scene->plane[pint.i].material;
+			intersection->distance = pint.t;
+			intersection->color = scene->plane[pint.i].color;
+			intersection->normal = scene->plane[pint.i].direction;
+			pint.tmp = vector_scale(&ray->direction, pint.t);
+			intersection->point = vector_add(&ray->origin, &pint.tmp);
+			cal_uv_basis(&scene->plane[pint.i].direction, &pint.u, &pint.v);
+			pint.hpr = vector_sub(&intersection->point,
+					&scene->plane[pint.i].position);
+			intersection->u = vector_dot(&pint.hpr, &pint.u) * 0.1;
+			intersection->v = vector_dot(&pint.hpr, &pint.v) * 0.1;
+		}
+	}
+	return (intersection->hit);
 }
