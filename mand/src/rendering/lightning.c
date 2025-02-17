93b760325a9579c3ed6c559ee445a21bac5da2e2
Soufiane Essarhir <sessarhi@e2r8p3.1337.ma>
2025-02-17 15:44:30 +0100

    handle inside objects intersection

--- mand/src/rendering/lightning.c
+++ mand/src/rendering/lightning.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:23:54 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/02/17 14:11:03 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/02/17 15:39:47 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,12 @@ int	pixel_color(t_scene *scene, t_intersection *intersection, t_ray *ray)
 	t_color_infos	coin;
 
 	coin.is_inside = vector_dot(&intersection->normal, &ray->direction) > 0;
-	 if (coin.is_inside)
+	 if (!coin.is_inside)
+    	coin.offset_direction = 1;
+    else
     {
         intersection->normal = vector_scale(&intersection->normal, -1);
         coin.offset_direction = -1;
-    }
-    else
-    {
-        coin.offset_direction = 1;
     }
 	coin.tmp_vector = vector_scale(&intersection->normal,
 			SHADOW_BIAS * coin.offset_direction);
